3b9894c09ef3aa806dc514ee8763cedbdc9715d3
alessandrofogliada <fogliada.a@gmail.com>
2024-09-24 15:07:34 +0200

    1.0.3 aggiunta nell'header il dropdown di home e logout

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,20 +1,28 @@
 {{-- questo sarà il menu piccolo in cima con le funzioni di tornare alla home del sito a sinistra ed a destra la possibilità di login/logout --}}
 <div class="row-12 text-bg-dark d-flex justify-content-between">
 
-    <div><a href="{{route('home')}}">Vai al sito</a></div>
+    <div><a href="{{route('home')}}" target="_blank">Vai al sito</a></div>
 
-    <ul class="navbar-nav ml-auto">
+    <ul class="navbar">
         @guest
-        <li><a href="{{route('login')}}"></a></li>
-        <li><a href="{{route ('register')}}"></a></li>
+            <li class="nav-item pe-3"><a href="{{route('login')}}"></a></li>
+            <li class="nav-item pe-3"><a href="{{route ('register')}}"></a></li>
         @else 
-        <li><a href="#"> {{ Auth::user()->name }} </a></li>
-        <li><a href="{{route ('admin.home')}}">Dashboard</a></li>
-        <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                @csrf
-            </form>
-      
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre> 
+                    {{ Auth::user()->name }} 
+                </a>
+                <div class="dropdown-menu dropdown-menu-left" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{route ('admin.home')}}">Admin</a>
+                    <<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+                    Logout
+                    </a>
+                </div>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </li>
         @endguest
     </ul>
 
